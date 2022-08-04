592c8589db3de3d7ec749664e52a1f5240b74927
Kiran Deep <kirandeep102030@gmail.com>
2022-08-04 15:59:16 +0530

    Added few code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <div className='flex justify-end bg-indigo-600 text-white text-lg'>
-        <ul className='flex my-3'>
+        <ul className='flex my-3 select-none'>
           <li className='mr-6 cursor-pointer'>About</li>
           <li className='mr-6 cursor-pointer'>Projects</li>
           <li className='mr-6 cursor-pointer'>Accomplishments</li>
